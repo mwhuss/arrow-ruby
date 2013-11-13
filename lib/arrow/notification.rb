50c24486ad7b53fc170bb168fba2a663f1578b56
Marshall Huss <mwhuss@gmail.com>
2013-11-13 14:23:56 -0500

    Added more attrs to notification

--- lib/arrow/notification.rb
+++ lib/arrow/notification.rb
@@ -1,18 +1,26 @@
 module Arrow
   class Notification
     
-    attr_accessor :token_address, :alert
+    attr_accessor :token_address, :alert, :badge, :sound, :content_available, :custom_data
     
     def initialize(opts = {})
-      @token_address = opts.delete(:token_address)
-      @alert = opts.delete(:alert)
+      @token_address        = opts.delete(:token_address)
+      @alert                = opts.delete(:alert)
+      @badge                = opts.delete(:badge)
+      @sound                = opts.delete(:sound)
+      @content_available    = opts.delete(:content_available)
+      @custom_data          = opts.delete(:custom_data)
     end
     
     def to_hash
-      {
-        token_address: @token_address,
-        alert: @alert
-      }
+      hash = { token_address: @token_address }
+      
+      hash[:alert]              = @alert if @alert
+      hash[:badge]              = @badge if @badge
+      hash[:sound]              = @sound if @sound
+      hash[:content_available]  = @content_available if @content_available
+      hash[:custom_data]        = @custom_data if @custom_data
+      hash
     end
     
   end
