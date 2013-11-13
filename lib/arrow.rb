c91e83cfd13060aff34f873fcf687f4bc8a4753c
Marshall Huss <mwhuss@gmail.com>
2013-11-13 10:25:18 -0500

    Added ClassMethods module

--- lib/arrow.rb
+++ lib/arrow.rb
@@ -3,29 +3,34 @@ require 'arrow/notification'
 
 module Arrow
   
-  def self.push(notifications)
-    notifications = Array(notifications)
-    params = { notifications: notifications.collect(&:to_hash) }
-    RestClient.post("#{self.server_url}/notifications", params.to_json, content_type: :json)
+  module ClassMethods
+    attr_accessor :server_url
+  
+    def push(notifications)
+      notifications = Array(notifications)
+      params = { notifications: notifications.collect(&:to_hash) }
+      RestClient.post("#{self.server_url}/notifications", params.to_json, content_type: :json)
+    end
+  
+    def broadcast_push(token_addresses, notif)
+      params = {
+        notification: notification.to_hash,
+        token_addresses: token_addresses
+      }
+      RestClient.post("#{self.server_url}/broadcast_notifications", params.to_json, content_type: :json)
+    end
+  
+    def feedback(app_uuid)
+      RestClient.get("#{self.server_url}/feedback/#{app_uuid}")
+    end
+  
+    def server_url
+      @server_url || ENV['ARROW_URL'] || raise("You must set Arrow.server_url or the ARROW_URL env var!")
+    end
   end
   
-  def self.broadcast_push(token_addresses, notif)
-    params = {
-      notification: notification.to_hash,
-      token_addresses: token_addresses
-    }
-    RestClient.post("#{self.server_url}/broadcast_notifications", params.to_json, content_type: :json)
+  class << self
+    include ClassMethods
   end
-  
-  def self.feedback(app_uuid)
-    RestClient.get("#{self.server_url}/feedback/#{app_uuid}")
-  end
-  
-  def self.server_url
-    url = ENV['ARROW_URL']
-    raise "You must set ARROW_URL!" unless url
-    url
-  end
-  
   
 end
